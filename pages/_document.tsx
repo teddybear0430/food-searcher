9b7201b7927fbf0afba524b6d03a3807e60b502d
teddybear0430 <teddybear0430@hotmail.com>
2023-01-08 02:23:30 +0900

    feat: Added ga tag.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,20 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="ja">
       <Head />
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-RXQM1E30LE" />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-RXQM1E30LE');`,
+        }}
+      />
       <body>
         <Main />
         <NextScript />
